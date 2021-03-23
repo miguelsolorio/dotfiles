246173aa58720807c61b6bba94452dbbd9daaaa4
Miguel Solorio <miguel.solorio@microsoft.com>
2021-03-23 09:21:32 -0700

    Init

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
+# And also installs Homebrew Packages
+# And sets Sublime preferences
+############################
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: install.sh <home_directory>"
+    exit 1
+fi
+
+homedir=$1
+
+# dotfiles directory
+dotfiledir=${homedir}/dotfiles
+
+# list of files/folders to symlink in ${homedir}
+files="bash_profile aliases"
+
+# change to the dotfiles directory
+echo "Changing to the ${dotfiledir} directory"
+cd ${dotfiledir}
+echo "...done"
+
+# create symlinks (will overwrite old dotfiles)
+for file in ${files}; do
+    echo "Creating symlink to $file in home directory."
+    ln -sf ${dotfiledir}/.${file} ${homedir}/.${file}
+done
